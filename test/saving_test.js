1ca665cb5b5c792197defe5b2ecfdb1ae521cd08
myName <xujing_xj@foxmail.com>
2020-05-04 19:52:52 +0800

    通过 id 获取数据

--- test/saving_test.js
+++ test/saving_test.js
@@ -2,25 +2,26 @@ const mocha = require("mocha");
 const assert = require("assert"); // 自带的, 引入断言模块
 
 const MarioChar = require("../models/mariochar");
+let char;
 
-/**
- * 测试哪个数据库
- * @param 参数1 写测试哪个 参数2
- */
-// 测试时对当前测试的一个描述
 describe("saving test", function () {
-    // 创建需要测试的任务
-    // 参数1 任务名
     it("数据存储成功", function () {
         // 准备需要存储的数据
-        let char = new MarioChar({
-            name: "Mario",
+        char = new MarioChar({
+            name: "money",
         });
 
         // 存储数据
         char.save().then(function (result) {
             // 注意connection.js加一句Promise
             assert(result.isNew === false);
+        });
+    });
+
+    it("finding data by id", function (done) {
+        MarioChar.findOne({ _id: char._id }).then((result) => {
+            console.log("by id----", result);
+            done();
         });
     });
 });
